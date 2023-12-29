f666b931108b26e999646c8ba85d68da74870e0f
Nataliia Tsirul <tcirulnatalya@gmail.com>
2023-12-29 13:31:19 +0000

    added MUI theme, provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+
+import CssBaseline from '@mui/material/CssBaseline';
+
+import { ThemeProvider } from '@mui/material/styles';
+import theme from '@/lib/theme/index';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <CssBaseline />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
